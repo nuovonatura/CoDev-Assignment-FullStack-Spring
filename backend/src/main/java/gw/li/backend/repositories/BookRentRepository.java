a22b61bbf58edeca777743348cd0aeae15049c3b
nuovonatura <nuovonatura@outlook.com>
2023-03-28 19:06:57 +0800

    Update the query to return valid results where count is larger than 1.

--- backend/src/main/java/gw/li/backend/repositories/BookRentRepository.java
+++ backend/src/main/java/gw/li/backend/repositories/BookRentRepository.java
@@ -14,14 +14,16 @@ import java.util.List;
 public interface BookRentRepository extends JpaRepository<BookRent, Long> {
     @Query(value = "SELECT b FROM " +
             "(SELECT br.id.book_id AS book_id FROM BookRent br " +
-            "GROUP BY br.id.book_id ORDER BY COUNT(br.id.book_id) DESC LIMIT 3) l " +
+            "GROUP BY br.id.book_id HAVING COUNT(br.id.book_id) >= 1 " +
+            "ORDER BY COUNT(br.id.book_id) DESC LIMIT 3) l " +
             "JOIN Book b ON l.book_id = b.id")
     List<Book> findTop3RentedBooks();
 
     @Query(value = "SELECT p FROM " +
             "(SELECT br.id.person_id AS person_id, COUNT(br.id.person_id) AS count " +
             "FROM BookRent br WHERE br.id.book_id = :book_id " +
-            "GROUP BY br.id.person_id ORDER BY COUNT(br.id.person_id) DESC) l " +
+            "GROUP BY br.id.person_id HAVING COUNT(br.id.person_id) >= 1 " +
+            "ORDER BY COUNT(br.id.person_id) DESC) l " +
             "JOIN Person p ON l.person_id = p.id WHERE p.country_id = :country_id ORDER BY l.count LIMIT 3")
-    List<Person> findTop3PeopleByBookAndCountry(@Param("book_id") int book_id, @Param("country_id") Long country_id);
+    List<Person> findTop3PeopleByBookAndCountry(@Param("book_id") int book_id, @Param("country_id") long country_id);
 }
