8daefedf3abf72de7b6a4e1dbd0b65691bcd0473
nuovonatura <nuovonatura@outlook.com>
2023-03-31 15:00:59 +0800

    Frontend optimization.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,28 @@ import ResultHolder from './components/ResultHolder';
 import { useState } from 'react';
 
 function App() {
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
+    const [country_code, setCountryCode] = useState("null");
 
-  const handleButtonClick = async () => {
+    const handleButtonClick = async () => {
+        const response = await fetch('http://localhost:8080/getRandomCountry');
+        const country = await response.json();
+        setCountryCode(country.country.full_name);
+        const response2 = await fetch('http://localhost:8080/getTop3ReadBooks?country_code=' + country.country.full_name);
+        const data = await response2.json();
+        setData(data);
+    }
 
-  }
-
-  return (
-    <div className="App">
-      <button
-      id='action-btn'
-      onClick={handleButtonClick}>
-        Get country:
-      </button>
-      <ResultHolder data={data}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <button
+            id='action-btn'
+            onClick={handleButtonClick}>
+                Get country: {country_code}
+            </button>
+            <ResultHolder data={data}/>
+        </div>
+    );
 }
 
 export default App;
